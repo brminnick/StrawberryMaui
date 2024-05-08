ba75179ab1e565beb70de880647a1606817501ac
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-05-08 12:26:48 +0200

    Remove existing code

--- StrawberryMaui/Services/GraphQLService.cs
+++ StrawberryMaui/Services/GraphQLService.cs
@@ -10,23 +10,6 @@ class GraphQLService(IContactsClient contactsClient)
 
 	public async Task<FrozenSet<Person>> GetContacts(CancellationToken token)
 	{
-		var response = await _contactsClient.GetAllPeople.ExecuteAsync(token).ConfigureAwait(false);
-		response.EnsureNoErrors();
-
-		var contactList = response.Data?.ListPeople?.Items?.Where(x => x != null).Select(x =>
-		{
-			ArgumentNullException.ThrowIfNull(x);
-
-			return new Person
-			{
-				Email = x.Email,
-				BirthDate = Convert.ToDateTime(x.Birthdate),
-				Id = x.Id,
-				Name = x.Name
-			};
-
-		}).ToFrozenSet() ?? Array.Empty<Person>().ToFrozenSet();
-
-		return contactList;
+		
 	}
 }
